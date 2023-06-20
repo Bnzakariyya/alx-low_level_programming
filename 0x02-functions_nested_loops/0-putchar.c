699cf08843eafd64067566176c537db305665473
Bnzakariyya <zakariyauabubakarsadiq@gmail.com>
2023-06-20 11:49:14 -0700

    updated

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,16 @@
 #include <unistd.h>
 
-void print_alphabet(void) {
-    char c = 'a';
-    while (c <= 'z') {
-        _putchar(c);
-        c++;
+int main(void) {
+    const char* message = "_putchar\n";
+    const size_t length = 9;  // Length of the message excluding the null terminator
+    
+    ssize_t bytes_written = write(STDOUT_FILENO, message, length);
+    if (bytes_written != length) {
+        // Handle error if the full message couldn't be written
+        return 1;
     }
-    _putchar('\n');
+    
+    return 0;
 }
+
 
