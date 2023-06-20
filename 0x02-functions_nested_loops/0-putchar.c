86b8ed78d0d95a3c127bbc5cbe63593aad5468c5
Bnzakariyya <zakariyauabubakarsadiq@gmail.com>
2023-06-20 11:55:37 -0700

    Updated

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,12 @@
-#include <unistd.h>
+#include "main.h"
 
-int main(void) {
-    const char* message = "_putchar\n";
-    const size_t length = 9;  // Length of the message excluding the null terminator
-    
-    ssize_t bytes_written = write(STDOUT_FILENO, message, length);
-    if (bytes_written != length) {
-        // Handle error if the full message couldn't be written
-        return 1;
-    }
-    
-    return 0;
+/**
+ * main - This program prints _putchar
+ *
+ * Return: 0 if program is successful
+ */
+int main(void)
+{
+	_putchar('_putchar'/n);
+	return (0);
 }
-
-
