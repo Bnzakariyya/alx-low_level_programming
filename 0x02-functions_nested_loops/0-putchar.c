93677788291d944ec14e1052506640d1d6370384
Bnzakariyya <zakariyauabubakarsadiq@gmail.com>
2023-06-20 15:25:01 -0700

    Doing hard things

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,6 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- * main - This program prints _putchar
- *
- * Return: 0 if program is successful
- */
-int main(void)
-{
-	_putchar('_putchar'/n);
-	return (0);
+int main() {
+    printf("_putchar\n");
+    return 0;
 }
