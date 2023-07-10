967f2373e4bb593787f68013430b2b8c48b80d5a
Bnzakariyya <zakariyauabubakarsadiq@gmail.com>
2023-07-09 18:57:11 -0700

    Silence is argument carried out by other means

---
+++ 0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main -name of function
+ * @argc: offset count
+ * @argv: offset value
+ * Return: 0
+ */
+
+
+
+
+int main(int argc, char *argv[])
+
+{
+
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+
+}
